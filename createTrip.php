e060b5bf644b3d722a23d35232d83694ec50d3b3
luffykesh <rushikesh@Rushikeshs-Macbook-Air.local>
2017-04-01 15:20:25 +0530

    json format added

--- createTrip.php
+++ createTrip.php
@@ -8,12 +8,16 @@ $con = mysqli_connect('localhost',USER,PASS,DB);
 
 $uuid= mysqli_real_escape_string($con, $_POST['uuid']);
 $vehicleid= mysqli_real_escape_string($con, $_POST['vehicleid']);
-$triplocation= mysqli_real_escape_string($con, $_POST['triploaction']);
+$triplocation= mysqli_real_escape_string($con, $_POST['triplocation']);
 $istriplive= mysqli_real_escape_string($con, $_POST['istriplive']);
 
 $sql = "SELECT COUNT(*) FROM tb_usertrips WHERE uuid='$uuid';";
 
 $tripid=$sql+1;
+
+$triplocationJsonarr = array("locations"=>$triplocation);
+
+triplocation=json_encode($triplocationJsonarr);
 
 $sql = "INSERT INTO tb_usertrips (UUID,TripID,Duration,VehicleID,TripLocation,isTripLive) VALUES('$uuid',$tripid,0,$vehicleid,'$triplocation',$istriplive);";
 
@@ -21,5 +25,7 @@ $stmt = mysqli_prepare($con,$sql);
 mysqli_stmt_execute($stmt);
 
 mysqli_close($con);
+
+echo $tripid;
 
 ?>
