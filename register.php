bdeeb3e750eb2cf2783818f6158511e3bb041a71
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 03:52:05 +0530

    Update register.php

--- register.php
+++ register.php
@@ -12,7 +12,7 @@ $vehicleid=mysqli_real_escape_string($con,$_POST['vehicleid']);
 $password=mysqli_real_escape_string($con,$_POST['password']);
 
 $statement =mysqli_prepare($con,"Insert INTO tb_userinfo(uuid,username,password,licensenumber,vehicleid) VALUES (?, ?, ?, ?,?)");
-mysqli_stmt_bind_param($statement,"ssssi",$uuid,$username,$password,$licenseno,$vehicleid);
+mysqli_stmt_bind_param($statement,"sssss",$uuid,$username,$password,$licenseno,$vehicleid);
 mysqli_stmt_execute($statement);
 mysqli_stmt_close($statement);
 echo $uuid;
