92e0f2da9c8ecdc74ec31b9053e96a60cb65b3bc
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-01 18:36:31 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -7,8 +7,9 @@ define('DB','lids');
 $con = mysqli_connect(HOST,USER,PASS,DB);
 
 $vehicle_id=mysqli_real_escape_string($con, $_POST['vehicle_id']);
+$sql = "select TripLocation from location where vehicle_id in('107');"; 
 
-$sql = "select TripLocation from location where vehicle_id in('".$vehicle_id."');"; 
+//$sql = "select TripLocation from location where vehicle_id in('".$vehicle_id."');"; 
 $res = mysqli_query($con,$sql);
  
 $result = array();
