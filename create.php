a5fa598e6c4d91a262b05fefe303a8832e96853c
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-01 17:29:39 +0530

    Create create.php

---
+++ create.php
@@ -0,0 +1,31 @@
+<?php
+define('USER','root');
+define('PASS','');
+define('DB','lids');
+$requestJSONData = file_get_contents('php://input');
+$requestArray = json_decode($requestJSONData,true);
+print_r($requestArray);
+$con = mysqli_connect('localhost',USER,PASS,DB);
+$vehicleid = $requestArray["vehicleid"];
+$uuid = $requestArray["UUID"];
+$triplocation = $requestArray["triplocation"];
+$triplocation=json_encode($triplocation);
+echo $triplocation;
+$sql = "select COUNT(*) from tb_usertrips where uuid in('".$uuid."');"; 
+$res = mysqli_query($con,$sql);
+$tripid=$tripid+1;
+echo "tripid:".$tripid;
+
+$sql = "INSERT INTO tb_usertrips VALUES('".$uuid."','".$tripid."',0,'".$vehicleid."','".$triplocation."',true);";
+print $sql;
+$result=mysqli_query($con,$sql);
+if($result===TRUE)
+  print "db insert success";
+else
+{
+  // print mysqli_error($con);
+  print "db insert fail";
+}
+mysqli_close($con);
+// echo $tripid;
+?>
