73440f19204feabca2dda5e1a526c6ab91eb1b0a
Sohail Chamadia <sohailyc@gmail.com>
2017-04-01 22:07:58 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -12,7 +12,7 @@ $vehicleID=101;
 $sql = "select TripLocation from tb_usertrips where vehicleID in('".$vehicleID."');"; 
 $res = mysqli_query($con,$sql);
 $row=mysqli_fetch_row($res);
-echo ("%s (%s)\n", $row[0], $row[1]);
+print ("%s (%s)\n", $row[0], $row[1]);
 var_dump(json_decode($res,true));
  
 mysqli_close($con);
