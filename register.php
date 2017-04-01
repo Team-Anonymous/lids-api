6fd69728906e50b0f14b20226335de2e37275c5b
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-01 22:58:19 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,12 +2,12 @@
 	$con=mysqli_connect("localhost","root","","lids");
 
 $name=$_POST("uuid");
-$age=$_POST("username");
-$username=$_POST("licenseno");
-$vehicleno=$_POST("vehicleid");
+$username=$_POST("username");
+$licenseno=$_POST("licenseno");
+$vehicleid=$_POST("vehicleid");
 $password=$_POST("password");
-$statement =mysqli_prepare($con,"Insert INTO tb_userinfo(uuid,username,licenseno,vehicleid,password) VALUES (?, ?, ?, ?)");
-mysqli_stmt_bind_param($statement,"sssi",$uuid,$username,$licenseno,$vehicleid,$password);
+$statement =mysqli_prepare($con,"Insert INTO tb_userinfo(uuid,username,password,licenseno,vehicleid) VALUES (?, ?, ?, ?,?)");
+mysqli_stmt_bind_param($statement,"ssssi",$uuid,$username,$password,$licenseno,$vehicleid);
 mysqli_stmt_execute($statement);
 mysqli_stmt_close($statement);
 mysqli_close($con);
