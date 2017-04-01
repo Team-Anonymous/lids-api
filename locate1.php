fc8bc797f38171cef4c7b46330a36cc92ea20073
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-01 13:34:12 +0530

    Add files via upload

---
+++ locate1.php
@@ -0,0 +1,26 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB','lids');
+
+$con = mysqli_connect(HOST,USER,PASS,DB);
+
+$vehicle_id=mysqli_real_escape_string($con, $_POST['vehicle_id']);
+
+$sql = "select TripLocation from location where vehicle_id in('".$vehicle_id."');"; 
+$res = mysqli_query($con,$sql);
+ 
+$result = array();
+ 
+while($row = mysqli_fetch_array($res)){
+array_push($result,
+array('latitude'=>$row[0],
+'longitude'=>$row[1]
+));
+}
+ 
+echo json_encode(array("result"=>$result));
+ 
+mysqli_close($con);
+?>
