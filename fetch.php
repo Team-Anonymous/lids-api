93cc1419af5318f18308fb5c733a82f1371db7fd
luffykesh <rushikesh@Rushikeshs-Macbook-Air.local>
2017-04-02 04:17:35 +0530

    fetch using only tripid

--- fetch.php
+++ fetch.php
@@ -4,19 +4,29 @@ define('USER','root');
 define('PASS','');
 define('DB','lids');
 
+if(isset($_GET["uuid"]))
+	$uuid=$_GET["uuid"];
+if(isset($_GET["tripid"]))
+	$tripid=$_GET["tripid"];
+
+// echo $tripid;
+
+if(isset($uuid))
+	echo "hello";
+
+if(isset($tripid))
+	$sql = "select * from tb_usertrips_loc where TripID = ".$tripid.";"; 	
+
 $con = mysqli_connect(HOST,USER,PASS,DB);
-
-$vehicleID=101;
-//$sql = "select TripLocation from location where vehicle_id in('107');"; 
-
-$sql = "select TripLocation from tb_usertrips where vehicleID in('".$vehicleID."');"; 
 $res = mysqli_query($con,$sql);
+$row = mysqli_fetch_assoc($res);
 $fin=array();
-while($data=mysqli_fetch_row($res))
+while($data=mysqli_fetch_assoc($res))
 {
-  array_push($fin,json_decode($data[0]));
+  array_push($fin,$data);
   //print($data[0]);
 }
 echo json_encode($fin);
+// print(json_encode($row));
 mysqli_close($con);
 ?>
