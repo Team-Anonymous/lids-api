aa2d2b76aa50fd5bcfc47b895207f4b7b9641d37
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 03:38:44 +0530

    Update register.php

--- register.php
+++ register.php
@@ -5,11 +5,11 @@
 //$licenseno=$_POST('licenseno');
 //$vehicleid=$_POST('vehicleid');
 //$password=$_POST('password');
-$uuid=mysli_real_escape_string($con,$_POST['uuid']);
-$username=mysli_real_escape_string($con,$_POST['username']);
-$licenseno=mysli_real_escape_string($con,$_POST['licenseno']);
-$vehicleid=mysli_real_escape_string($con,$_POST['vehicleid']);
-$password=mysli_real_escape_string($con,$_POST['password']);
+$uuid=mysqli_real_escape_string($con,$_POST['uuid']);
+$username=mysqli_real_escape_string($con,$_POST['username']);
+$licenseno=mysqli_real_escape_string($con,$_POST['licenseno']);
+$vehicleid=mysqli_real_escape_string($con,$_POST['vehicleid']);
+$password=mysqli_real_escape_string($con,$_POST['password']);
 
 $statement =mysqli_prepare($con,"Insert INTO tb_userinfo(uuid,username,password,licensenumber,vehicleid) VALUES (?, ?, ?, ?,?)");
 mysqli_stmt_bind_param($statement,"ssssi",$uuid,$username,$password,$licenseno,$vehicleid);
