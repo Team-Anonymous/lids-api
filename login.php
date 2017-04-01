6a029535a9c20bfcf875e7d2dfa2b68fb26c849c
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 04:50:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,9 +8,6 @@
     mysqli_stmt_bind_param($statement, "ss", $username, $password);
     mysqli_stmt_execute($statement);
     
-    mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $uuid, $username, $password);
-    
     $response = array();
     $response["success"] = false;  
     
