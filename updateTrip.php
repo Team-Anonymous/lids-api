bdf73dd265fb800e115ab5a7082b2073afdc082a
luffykesh <rushikesh@Rushikeshs-Macbook-Air.local>
2017-04-02 03:26:34 +0530

    update location in a trip

--- updateTrip.php
+++ updateTrip.php
@@ -8,7 +8,7 @@ $requestJSONData = file_get_contents('php://input');
 
 $requestArray = json_decode($requestJSONData,true);
 
-
+// print_r($requestArray);
 // parse json post request
 $vehicleid = $requestArray["vehicleid"];
 $uuid = $requestArray["uuid"];
@@ -19,6 +19,7 @@ $istripalive=$requestArray["istripalive"];
 $tripid=$requestArray["tripid"];
 $duration=$requestArray["duration"];
 
+$con = mysqli_connect('localhost',USER,PASS,DB);
 $sql="INSERT INTO tb_usertrips_loc VALUES($tripid,$latitudeE7,$longitudeE7,$timestampMs);";
 // print($sql."\n");
 $result=mysqli_query($con,$sql);
@@ -34,9 +35,17 @@ $result=mysqli_query($con,$sql);
 mysqli_close($con);
 
 $con = mysqli_connect('localhost',USER,PASS,DB);
-$sql="INSERT INTO tb_usertrips VALUES ('".$uuid."',$tripid,0,$vehicleid,'".$triplocation."',$istripalive)";
-$stmt = mysqli_prepare($con,$sql);
-mysqli_stmt_execute($stmt);
+$sql="UPDATE tb_usertrips SET isTripLive=$istripalive,duration=$duration WHERE TripID=$tripid;";
+// print($sql."\n");
+$result=mysqli_query($con,$sql);
+// if($result)
+// {
+// 	echo 'query successfull';
+// }
+// else 
+// {
+// 	echo "query fail";
+// }
 
 mysqli_close($con);
 
