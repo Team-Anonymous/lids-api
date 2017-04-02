88dd56a3b376cfdabbbccc816422400765efd455
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 13:04:43 +0530

    Update immobilize_command.php

--- immobilize_command.php
+++ immobilize_command.php
@@ -5,7 +5,11 @@ $immobilized=mysqli_real_escape_string($con,$_POST['immobilized']);
 $vehicleid=mysqli_real_escape_string($con,$_POST['vehicleid']);
 $licenseid=mysqli_real_escape_string($con,$_POST['licenseid']);
 $statement =mysqli_prepare($con,"update tb_vehiclecommand set immobilized=$immobilized where vehicleid=$vehicleid and licenseid=$licenseid );
-mysqli_stmt_execute($statement);
+if(mysqli_stmt_execute($statement))
+echo "Success";
+else 
+echo "Not immobilized !";
+
 mysqli_stmt_close($statement);
 mysqli_close($con);
 ?>
