5b4a9614d33e338bd765b357915fdfe668178387
Sohail Chamadia <sohailyc@gmail.com>
2017-04-02 06:09:39 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     $username = mysqli_real_escape_string($con,$_POST['username']);
     $password =mysqli_real_escape_string($con,$_POST['password']);
     $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE username = '$username' AND password = '$password'");
-    if(mysqli_stmt_fetch($statement)){
+    if(mysqli_num_rows($statement)>0){
           echo "success";
     }
     else{
