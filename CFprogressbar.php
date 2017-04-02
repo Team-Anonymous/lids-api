67d77a9493c2881b3bb4b68672717df3f46f6897
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 16:59:36 +0530

    Update CFprogressbar.php

--- CFprogressbar.php
+++ CFprogressbar.php
@@ -9,7 +9,7 @@ $sql = "SELECT CurrentCF FROM tb_pollutionquotient WHERE LicenseID='".$licenseid
 
 $con = mysqli_connect(HOST,USER,PASS,DB);
 $res = mysqli_query($con,$sql);
-$row=mysqli_fetch_asoc($res);
+$row=mysqli_fetch_assoc($res);
 $ans=$row['CurrentCF'];
 echo $ans;
 mysqli_close($con);
