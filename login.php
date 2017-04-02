923c335add8f18e0c6b311319d982d82f120f714
Sohail Chamadia <sohailyc@gmail.com>
2017-04-02 06:06:07 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,8 @@
 <?php
     $con = mysqli_connect("localhost", "root", "", "lids");
-    
     $username = mysqli_real_escape_string($con,$_POST['username']);
     $password =mysqli_real_escape_string($con,$_POST['password']);
-    
-    $statement = mysqli_prepare($con, "SELECT * FROM tb_userinfo WHERE username = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
-    mysqli_stmt_execute($statement);
-    
-  
+    $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE username = '$username' AND password = '$password'");
     if(mysqli_stmt_fetch($statement)){
           echo "success";
     }
