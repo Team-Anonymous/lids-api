d9fd5ee46b122036ce834228b22b0bcf192a4a9e
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-02 16:52:11 +0530

    Update CFprogressbar.php

--- CFprogressbar.php
+++ CFprogressbar.php
@@ -4,10 +4,7 @@ define('USER','root');
 define('PASS','');
 define('DB','lids');
 
-if(isset($_POST["licenseid"]))
-	$licenseid=$_POST["licenseid"];
-if(!isset($licenseid))
-	die("invalid query");
+$licenseid=$_POST["licenseid"];
 $sql = "SELECT CurrentCF FROM tb_pollutionquotient WHERE LicenseID='".$licenseid."';";
 
 $con = mysqli_connect(HOST,USER,PASS,DB);
