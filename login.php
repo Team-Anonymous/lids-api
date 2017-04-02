0ae55c4487c699f24fc49a74312f0f062c4e009b
Sohail Chamadia <sohailyc@gmail.com>
2017-04-02 06:37:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,11 +2,11 @@
     $con = mysqli_connect("localhost", "root", "", "lids");
     //$username = mysqli_real_escape_string($con,$_POST['username']);
     //$password =mysqli_real_escape_string($con,$_POST['password']);
-    $username=$_GET['username'];
-//$password=$_GET['password'];
+    $username=$_POST['username'];
+$password=$_POST['password'];
 echo $username;
-    //echo $password;
-    $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE UserName in ('".$username."')  ");
+    echo $password;
+    $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE UserName =$username AND password = $password ");
 print_r($statement);
     if(mysqli_num_rows($statement)>0){
           echo "success";
