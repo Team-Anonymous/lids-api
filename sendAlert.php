61e78c94c682b132691e0fdbc73d8c275082f55e
luffykesh <rushikesh@Rushikeshs-Macbook-Air.local>
2017-04-02 15:29:00 +0530

    added AlertPriority

--- sendAlert.php
+++ sendAlert.php
@@ -15,9 +15,10 @@ $violation = $requestArray["violation"];
 $number = $requestArray["number"];
 $action=$requestArray["action"];
 $timestampMs=$requestArray["timestampMs"];
+$alertpriority=$requestArray["alertpriority"];
 
 $con = mysqli_connect('localhost',USER,PASS,DB);
-$sql="INSERT INTO tb_alerts VALUES($timestampMs,$violation,'".$number."',$action);";
+$sql="INSERT INTO tb_alerts VALUES($timestampMs,$violation,'".$number."',$action,$alertpriority);";
 $res = mysqli_query($con,$sql);
 if($res)
 {
