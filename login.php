ec35c3e956feac210506f4cfe042de4eecbe2bef
Sohail Chamadia <sohailyc@gmail.com>
2017-04-02 06:31:40 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ echo $username;
     $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE UserName = '".$username."' AND password = '".$password."' ");
 print_r($statement);
     if(mysqli_num_rows($statement)>0){
-          echo "success
+          echo "success";
     }
     else{
         echo "authentication failed !";
