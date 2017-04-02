a39d6f826b255210e7a31f92a3b86348a461acd7
Sohail Chamadia <sohailyc@gmail.com>
2017-04-02 06:34:41 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     //$username = mysqli_real_escape_string($con,$_POST['username']);
     //$password =mysqli_real_escape_string($con,$_POST['password']);
     $username=$_GET['username'];
-$password=$_GET['password'];
+//$password=$_GET['password'];
 echo $username;
     //echo $password;
     $statement = mysqli_query($con, "SELECT * FROM tb_userinfo WHERE UserName = '".$username."'  ");
