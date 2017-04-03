bdf9030c4d7a38b7a5fd193bd8c85c7f50e6a218
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-04-03 13:30:25 +0530

    immobilize th vehicle
    success error resolved

--- immobilize_command.php
+++ immobilize_command.php
@@ -4,8 +4,14 @@
 $immobilized=mysqli_real_escape_string($con,$_POST['immobilized']);
 $vehicleid=mysqli_real_escape_string($con,$_POST['vehicleid']);
 $licenseid=mysqli_real_escape_string($con,$_POST['licenseid']);
-$statement =mysqli_prepare($con,"update tb_vehiclecommand set immobilized=$immobilized where vehicleid=$vehicleid and licenseid=$licenseid );
-mysqli_stmt_execute($statement);
+$statement =mysqli_prepare($con,"update tb_vehiclecommand set immobilized='$immobilized' where vehicleid='$vehicleid' and licenseid='$licenseid'" );
+
+if(mysqli_stmt_execute($statement)){
+echo 'Success';
+}
+else {
+echo 'Not immobilized !';
+}
 mysqli_stmt_close($statement);
 mysqli_close($con);
 ?>
